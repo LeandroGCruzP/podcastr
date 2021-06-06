8a56ffb3732f8acd570c049d56256007edd0930b
Leandro Geovanni Cruz <leandro.programmer@hotmail.com>
2021-06-05 21:53:52 -0400

    add Header and images

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import format from "date-fns/format";
+import ptBR from "date-fns/locale/pt-BR";
+
+import styles from "./styles.module.scss";
+
+export default function Header() {
+  /* Formatando a data 
+  Para formatar a data se ocupará o pacote "date-fns" 
+  e o formato escolhido foi a través da documentação do proprio date-fns/format
+  instalar: yarn add date-fns
+  link: https://date-fns.org/v2.22.1/docs/format
+  */
+  const currentDate = format(new Date(), "EEEEEE, d MMM", {
+    locale: ptBR, // idioma da data selecionada
+  });
+
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Podcastr" />
+
+      <p>O melhor para você ouvir, sempre</p>
+
+      <span>{currentDate}</span>
+    </header>
+  );
+}
