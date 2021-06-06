42f18eed267a2c8e297260faa10c3bbb0c981c6e
Leandro Geovanni Cruz <leandro.programmer@hotmail.com>
2021-06-06 13:05:14 -0400

    add component Player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,22 @@
 /* Teoria de _app
-Toda vez que a aplicação recarrega alguma página o _app recarrega novamente
+Toda vez que a aplicação recarrega alguma página o _app recarrega novamente mostrando assim esas páginas em todas telas
 Por este motivo se cria o _document pra, por exemplo: colocar as fontes de fonts.google.com */
-
 import "../styles/global.scss";
+import styles from "../styles/app.module.scss";
+
+import Header from "../components/Header";
+import Player from "../components/Player";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  );
 }
 
 export default MyApp;
